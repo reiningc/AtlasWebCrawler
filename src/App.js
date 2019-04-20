e2517bee1554047697e87a1ab8ec734680abe10c
Jessica Adams <jessados1987@gmail.com>
2019-04-20 22:24:19 +0900

    connected starter api index.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,35 @@ import './App.css';
 import TestComponent from './components/testcomponent';
 
 class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+  this.callBackendAPI()
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
+}
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+callBackendAPI = async () => {
+  const response = await fetch('/express_backend');
+  const body = await response.json();
+
+  if (response.status !== 200) {
+    throw Error(body.message) 
+  }
+  return body;
+};
+
+
   render() {
     return (
       <div className="App">
         <TestComponent></TestComponent>
+        <br/>
+          <p className="App-intro">{this.state.data}</p>
+        <br/>
         <form>
         <div class="form-group"> 
         <label for="website">Starting address for crawl:</label>
