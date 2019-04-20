a93132a79bc1161ef8fcdade236ac4fc089844e1
Jessica Adams <jessados1987@gmail.com>
2019-04-20 09:52:34 +0900

    added simple form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import TestComponent from './components/testcomponent';
-import Urlbox from './components/urlentry';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TestComponent></TestComponent>
-        <Urlbox></Urlbox>
+        <form>
+        <div class="form-group"> 
+        <label for="website">Starting address for crawl:</label>
+        <br/>
+            <input type="url" class="col-xs-4" id="website"/>
+        <br/>
+        <br/>
+        <label class="radio-inline">Select Search Type:</label>
+        <br/>
+        <label class="radio-inline">
+      <input type="radio" name="optradio" checked/>Breadth First Search
+      </label>
+      <label class="radio-inline">
+        <input type="radio" name="optradio" />Depth First Search
+      </label>
+       <br/>
+        <br/>
+        <div class="form-group">
+        
+          <label for="depth">Select Maximum Number of Links to Follow: </label>
+          <br/>
+          <input type="number" class="col-xs-4" id="depth" />
+        </div>
+        <br/>
+        <div class="col-xs-4">
+          <label for="safeword">Enter a word that when encountered will stop crawl:</label>
+          <br/>
+          <input type="text" class="col-xs-4" id="safeword" />
+          <br/>
+        </div>
+                </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
       </div>
     );
   }
