f83e75ff4356f0f1ea92234416f309fdfbfa1b2c
Colin Reininger <reiningc@oregonstate.edu>
2019-05-06 22:13:18 -0600

    Add empty sets to get_all_links() calls

--- scripts/test_parseHTML.py
+++ scripts/test_parseHTML.py
@@ -8,22 +8,22 @@ class Test(unittest.TestCase):
     
     def test_internal_site_link(self):
         test_text = '<a href="/events/python-events">'
-        result = crawler.parseHTML.get_all_links(test_text, 'http://python.org')
+        result = crawler.parseHTML.get_all_links(test_text, 'http://python.org', set())
         self.assertSetEqual(result, {'http://python.org/events/python-events'})
 
     def test_example_site(self):
         test_text = '<!doctype html>\n<html>\n<head>\n    <title>Example Domain</title>\n\n    <meta charset="utf-8" />\n    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1" />\n    <style type="text/css">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 50px;\n        background-color: #fff;\n        border-radius: 1em;\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        body {\n            background-color: #fff;\n        }\n        div {\n            width: auto;\n            margin: 0 auto;\n            border-radius: 0;\n            padding: 1em;\n        }\n    }\n    </style>    \n</head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is established to be used for illustrative examples in documents. You may use this\n    domain in examples without prior coordination or asking for permission.</p>\n    <p><a href="http://www.iana.org/domains/example">More information...</a></p>\n</div>\n</body>\n</html>\n'
-        result = crawler.parseHTML.get_all_links(test_text, 'http://example.com')
+        result = crawler.parseHTML.get_all_links(test_text, 'http://example.com', set())
         self.assertSetEqual(result, {'http://www.iana.org/domains/example'})
 
     def test_no_links(self):
         test_text = '<!doctype html>\n<html>\n<head>\n    <title>Example Domain</title>\n\n    <meta charset="utf-8" />\n    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1" />\n    <style type="text/css">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 50px;\n        background-color: #fff;\n        border-radius: 1em;\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        body {\n            background-color: #fff;\n        }\n        div {\n            width: auto;\n            margin: 0 auto;\n            border-radius: 0;\n            padding: 1em;\n        }\n    }\n    </style>    \n</head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is established to be used for illustrative examples in documents. You may use this\n    domain in examples without prior coordination or asking for permission.</p>\n    <p>More information...</p>\n</div>\n</body>\n</html>\n'
-        result = crawler.parseHTML.get_all_links(test_text, 'http://example.com')
+        result = crawler.parseHTML.get_all_links(test_text, 'http://example.com', set())
         self.assertSetEqual(result,set())    
 
     def test_only_internal_links(self):
         test_text = '<!doctype html>\n<html>\n<head>\n    <title>Example Domain</title>\n\n    <meta charset="utf-8" />\n    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1" />\n    <style type="text/css">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 50px;\n        background-color: #fff;\n        border-radius: 1em;\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        body {\n            background-color: #fff;\n        }\n        div {\n            width: auto;\n            margin: 0 auto;\n            border-radius: 0;\n            padding: 1em;\n        }\n    }\n    </style>    \n</head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <a href="#bottom">Bottom</a><p>This domain is established to be used for illustrative examples in documents.\n <a href="#top>Top</a> You may use this\n    domain in examples without prior coordination or asking for permission.</p>\n    <p><a href="#example">More information...</a></p>\n</div>\n</body>\n</html>\n'
-        result = crawler.parseHTML.get_all_links(test_text, 'http://example.com')
+        result = crawler.parseHTML.get_all_links(test_text, 'http://example.com', set())
         self.assertSetEqual(result,set())
     
     def test_relative_URL_one_level_up(self):
@@ -54,6 +54,5 @@ class Test(unittest.TestCase):
         URL = 'http://www.iana.org/gobs/dobz/rfc2606'
         result = crawler.parseHTML.parse_base_URL(URL)
         self.assertEqual(result, 'http://www.iana.org')
-    
 
 unittest.main(verbosity=2)
