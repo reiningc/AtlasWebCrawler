b7460f30a79c39d901c76d1e080c55eee7fa0eb0
bearados <jessados1987@gmail.com>
2019-05-16 15:33:20 +0900

    moved front end files to separate directory

---
+++ index.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const port = process.env.PORT || 5000;
+var path = require('path');
+
+
+
+
+// create a GET route
+app.get('/', (req, res) => {
+  res.send("ok");
+});
+
+
+app.post('/', (req, res)=>{
+  console.log(req.body);
+  var spawn = require("child_process").spawn;
+  const depthCrawl = spawn('python',["./AtlasWebCrawler/scripts/df_crawl.py", 
+  req.body.website, req.body.depth]);
+  depthCrawl.on('exit', function (code, signal) {
+    depthCrawl.stdout.pipe(process.stdout);
+    console.log('child process exited');
+    res.sendFile(path.join(__dirname, '/AtlasWebCrawler/scripts/logs', 'crawl.log'));
+  });
+  
+  
+});
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'AtlasWebCrawler/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'AtlasWebCrawler/build', 'index.html'));
+  });
+}
+
+
+app.use(function (req, res) {
+  res.status(404);
+  res.send('404');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.send('500');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+});
+
