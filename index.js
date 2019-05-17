55f2ced15c23759f4d76ef8953c6ab48a47570d6
bearados <jessados1987@gmail.com>
2019-05-17 12:57:52 +0900

    added server files

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 5000;
 var path = require('path');
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 
 // create a GET route
 app.get('/', (req, res) => {
-  res.render("index");
+  res.render("index.html");
 });
 
 
-app.post('/', (req, res)=>{
-  console.log(req.body);
+app.post('/dfs', (req, res)=>{
+  
+  console.log(req.body.website);
   var spawn = require("child_process").spawn;
   const depthCrawl = spawn('python',["./scripts/df_crawl.py", 
-  req.body.website, req.body.depth]);
+  req.body.website, req.body.depth, req.body.depth]);
   depthCrawl.on('exit', function (code, signal) {
     depthCrawl.stdout.pipe(process.stdout);
     console.log('child process exited');
@@ -33,6 +33,31 @@ app.post('/', (req, res)=>{
   
 });
 
+
+app.post('/bfs', (req, res)=>{
+  
+  console.log(req.body.param.website);
+  var spawn = require("child_process").spawn;
+  const depthCrawl = spawn('python',["./scripts/bf_crawl.py", 
+  req.body.param.website, req.body.param.depth, req.body.param.keyword]);
+  depthCrawl.on('exit', function (code, signal) {
+    depthCrawl.stdout.pipe(process.stdout);
+    console.log('child process exited');
+    res.sendFile(path.join(__dirname, '/scripts/logs', 'crawl.log'));
+  });
+  
+  
+});
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 
 app.use(function (req, res) {
