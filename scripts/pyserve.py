8b164a641b8464dcfe1380684d490a361cb009d8
bearados <jessados1987@gmail.com>
2019-05-21 14:22:32 +0900

    python stuff

--- scripts/pyserve.py
+++ scripts/pyserve.py
@@ -5,21 +5,24 @@ import os
 from kombu import Connection, Exchange, Queue, Consumer
 import testfunc
 
-video_queue = Queue('tasks')
+crawl_queue = Queue('tasks')
 print("pyserve: starting connection to cloudamqp ... ")
 rabbit_url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost//')
+
+def on_request(body, message):
+    n = int(body)
+    print("running testfunc")
+    response = testfunc.add1(n)
+    print("results: %d" + str(response))
+
+
 with Connection(rabbit_url) as conn:
-    with conn.Consumer(video_queue, callbacks=[on_request]) as consumer:
+    with conn.Consumer(crawl_queue, callbacks=[on_request]) as consumer:
         # Process messages and handle events on all channels
         while True:
             conn.drain_events()
 
 
 
-def on_request(body, message):
-    n = int(body)
 
-    print("running testfunc")
-    response = testfunc.add1(n)
-    print("results: %d" + str(response))
     
