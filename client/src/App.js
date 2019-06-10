bf080706d9b08d6947f6a7be1b20cd8f01119330
Colin Reininger <reiningc@oregonstate.edu>
2019-06-09 21:17:47 -0600

    Set up socket.io-client

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,28 @@ import Test from './screens/Test';
 
 import history from './history'
 
+import socketIOClient from "socket.io-client";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: process.env.HOST+":"+process.env.PORT
+    };
+  }
 
+  componentDidMount(){
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on('found', (data) => {
+      console.log('in react "found" listener. received data: '+ data);
+      this.setState({data: data.json(), loading: false});
+      socket.emit('confirmed', '0');
+      console.log('react found listener emits "confirmed"');
+    });
+  }
+  
   render() {
     return (
       <Router history={history}>
